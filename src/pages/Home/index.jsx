39bbf34fed717a4fa76138e4c949899bef8a0254
zhubenlei <zhubenlei@bytedance.com>
2019-11-29 16:04:24 +0800

    babel-eslint

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,18 @@ import React,{PureComponent} from "react";
 import "./style.less";
 
 class Home extends PureComponent{
-  constructor(props){
-    super(props);
-    this.state={
-
-    };
+  
+  state={
+    name:"namexxx"
   }
+
+  handleClick=()=>{
+    console.log("onclick this.state.name-------->",this.state.name);
+  }
+
   render(){
     return <div>
-      <h1>Home</h1>
+      <h1 onClick={this.handleClick}>Home</h1>
     </div>;
   }
 }
