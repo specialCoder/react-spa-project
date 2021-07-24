a8e4a176ccf8fcd9baccb2d60427795f28ba827d
zhubenlei <zhubenlei@fenbi.com>
2021-07-24 20:03:41 +0800

    chore: fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,6 @@ All notable changes to this project will be documented in this file. See [standa
 
 ### 0.0.3 (2021-07-24)
 
-
 ### Features
-
-* 完善模版 ([b821c70](https://gerrit.zhenguanyu.com:29418///commit/b821c7045de46806971885941f1bc28f702d9da7))
 
 ### 0.0.2 (2021-04-20)
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,23 @@
-const path = require("path");
+const path = require('path');
 const webpack = require('webpack');
-const { merge } = require("webpack-merge");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {
+  merge
+} = require('webpack-merge');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // const CopyWebpackPlugin = require('copy-webpack-plugin');
-const common = require("./webpack.common.js");
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode:"development",
-  plugins:[
+  mode: 'development',
+  plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "App Demo", // 指定html文件的title标签内容
-      template:path.resolve(__dirname,"./static/html/template.dev.html"), // 指定要使用的模版
+      title: 'App Demo', // 指定html文件的title标签内容
+      template: path.resolve(__dirname, './static/html/template.dev.html'), // 指定要使用的模版
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
@@ -21,17 +25,17 @@ module.exports = merge(common, {
     // webpack bundle analyzer
     // new BundleAnalyzerPlugin(),
   ],
-  devtool: "inline-source-map",
-  devServer:{
-    contentBase: "./dist",
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
     compress: true,
     disableHostCheck: true, // 去掉域名访问检验，可以通过自定义的域名访问
-    proxy:{
-      '/api/**':{
-        target: 'https://conan.zhenguanyu.com'
+    proxy: {
+      '/api/**': {
+        target: 'https://proxy.com'
       }
     },
-    port:3000,
+    port: 3000,
     historyApiFallback: true,
   }
 });
